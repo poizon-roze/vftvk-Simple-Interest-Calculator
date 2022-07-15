4499b185f95a6bd12fe08bf1281ecee3a4e2db57
poizon-roze <75308080+poizon-roze@users.noreply.github.com>
2022-07-15 22:59:29 +0900

    manual number drop down
    i don't use js for dropdown and change the code to show range value. basically the same

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 
-function updateTextInput(val) {
-          document.getElementById('textInput').value=val; 
-        }
+
+function rate_value() {
+    var rate_val = document.getElementById("rate_val");
+    var rate = document.getElementById("rate").value;
+    rate_val.innerHTML = rate;
+} 
+// function updateTextInput(val) {
+//           document.getElementById('textInput').value=val; 
+//         }
         
-window.onload = function () {
+// window.onload = function () {
         //Reference the DropDownList.
-        years = document.getElementById("years");
+        // years = document.getElementById("years");
  
         //Determine the Current Year.
         // var currentYear = (new Date()).getFullYear();
  
         //Loop and add the Year values to DropDownList.
-        for (var i = 1; i <= 10; i++) {
-            var option = document.createElement("OPTION");
-            option.innerHTML = i;
-            option.value = i;
-            years.appendChild(option);
-        }
-    };
-
-
+    //     for (var i = 1; i <= 10; i++) {
+    //         var option = document.createElement("OPTION");
+    //         option.innerHTML = i;
+    //         option.value = i;
+    //         years.appendChild(option);
+    //     }
+    // };
 function compute() {
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
     var result = document.getElementById("result");
+
     var yield = principal * rate * years / 100;
-
-    result.innerHTML = "If you deposit" + principal + "at an interest rate of" + rate + "%." + "You will receive an amount of" + yield;
-
-
+    // result.innerHTML = "If you deposit" + principal + "at an interest rate of" + rate + "%." + "You will receive an amount of" + yield;
+    result.innerHTML = yield;
+}
 
     // if (principal <= 0) {
     //     result.innerHTML = "please put positive value";
@@ -41,7 +45,7 @@ function compute() {
     //     var yield = principal * rate * years / 100;
     //     result.innerHTML = yield;
     // }
-}
+// }
 
 
 
