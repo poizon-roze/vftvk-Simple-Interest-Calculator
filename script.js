cf8ac5e9e6672acb04dd8234457871444fd6acb1
poizon-roze <75308080+poizon-roze@users.noreply.github.com>
2022-07-16 12:29:01 +0900

    all works
    finally all works with type="button"

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-
+function clean() {
+document.getElementById("rate_val").innerHTML = "10.25"
+document.getElementById("principal").focus()
+document.getElementById("result").innerHTML = ""
+}
 
 function rate_value() {
-    var rate_val = document.getElementById("rate_val");
-    var rate = document.getElementById("rate").value;
+    let rate_val = document.getElementById("rate_val");
+    let rate = document.getElementById("rate").value;
     rate_val.innerHTML = rate;
 } 
 // function updateTextInput(val) {
@@ -25,27 +29,22 @@ function rate_value() {
     //     }
     // };
 function compute() {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    var result = document.getElementById("result");
+    let principal = document.getElementById("principal").value;
+    let years = document.getElementById("years").value;
+    let result = document.getElementById("result");
+    let rate = document.getElementById("rate").value;
 
-    var yield = principal * rate * years / 100;
-    // result.innerHTML = "If you deposit" + principal + "at an interest rate of" + rate + "%." + "You will receive an amount of" + yield;
-    result.innerHTML = yield;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if (principal < 0 || isNaN(principal) || !principal) {
+        alert("please put positive value")
+    }
+    
+    else {
+        var yield = principal * rate * years / 100;
+        result.innerHTML = "If you deposit " + principal + ", an interest rate of " + rate + "%." + "You will receive an amount of " + yield + ". In the year " + year
+    }
 }
-
-    // if (principal <= 0) {
-    //     result.innerHTML = "please put positive value";
-    // }
-    // else if (!principal || !rate) {
-    //     result.innerHTML = "please put value";
-    // } 
-    // else {
-    //     var yield = principal * rate * years / 100;
-    //     result.innerHTML = yield;
-    // }
-// }
 
 
 
